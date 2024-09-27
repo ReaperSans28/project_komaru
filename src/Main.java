b3f9a631f020cfd91e5eb4c92422ab83440f13be
ReaperSans28 <smileuwu321@gmail.com>
2024-09-27 11:30:09 +0300

    hw3

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,86 @@
  class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
 
-        var heavyDog = dog + 4;
-        var heavyCat = cat + 4;
-        var heavyPaper = paper + 4;
-        System.out.println(heavyDog);
-        System.out.println(heavyCat);
-        System.out.println(heavyPaper);
+        int pyaterka = 55555;
+        byte minecraft = 16;
+        short mista = 4444;
+        long sleepers = 999999999999999999L;
+        float pi = 3.14f;
+        double pi2 = 3.1415926535897932384626433832795028841971;
 
-        var otherDog = heavyDog - 3.5;
-        var otherCat = heavyCat - 1.6;
-        var otherPaper = heavyPaper - 7639;
-        System.out.println(otherDog);
-        System.out.println(otherCat);
-        System.out.println(otherPaper);
+        float numb = 27.12f;
+        long druid = 987678965549L;
+        double a = 2.786;
+        short b = 569;
+        short minus = -159;
+        short plus = 27897;
+        byte c = 67;
 
-        var friend = 19;
-        System.out.println(friend);
-        var biggerFriend = friend + 2;
-        System.out.println(biggerFriend);
-        var smallerFriend = biggerFriend / 7;
-        System.out.println(smallerFriend);
+        var LudmilaPavlovnasClass = 23;
+        var AnnaSergeevnasClass = 27;
+        var EkaterinaAndreevna = 30;
+        var totalPaper = 480;
+        var totalStudents = LudmilaPavlovnasClass + AnnaSergeevnasClass + EkaterinaAndreevna;
+        var paperForOneStudent = totalPaper / totalStudents;
+        System.out.println(paperForOneStudent);
 
-        var frog = 3.5;
-        System.out.println(frog);
-        var biggerFrog = frog * 10;
-        System.out.println(biggerFrog);
-        var smallestFrog = biggerFrog / 3.5;
-        System.out.println(smallestFrog);
-        var smallFrog = smallestFrog + 4;
-        System.out.println(smallFrog);
+        var bottlesPerTwoMinutes = 16;
+        var bottlesPerMinute = bottlesPerTwoMinutes / 2;
+        var bottlesPerTwentyMinutes = bottlesPerTwoMinutes * 20;
+        var bottlesPerDay = bottlesPerMinute * 1440;
+        var bottlesPerThreeDays = bottlesPerDay * 3;
+        var bottlesPerMonth = bottlesPerThreeDays * 10;
+        System.out.println("За 20 минут машина произвела " + bottlesPerTwentyMinutes + " штук бутылок");
+        System.out.println("За 20 минут машина произвела " + bottlesPerDay + " штук бутылок");
+        System.out.println("За 20 минут машина произвела " + bottlesPerThreeDays + " штук бутылок");
+        System.out.println("За 20 минут машина произвела " + bottlesPerMonth + " штук бутылок");
 
-        var firstBoxer = 78.2;
-        var secondBoxer = 82.7;
-        var totalMass = firstBoxer + secondBoxer;
-        System.out.println(totalMass);
-        var differenceMass = secondBoxer - firstBoxer;
-        System.out.println(differenceMass);
+        int totalCans = 120;
+        int whitePerClass = 2;
+        int brownPerClass = 4;
+        int totalClasses = totalCans / (whitePerClass + brownPerClass);
+        int totalWhiteCans = totalClasses * whitePerClass;
+        int totalBrownCans = totalClasses * brownPerClass;
+        System.out.println("В школе, где " + totalClasses + " классов, нужно " + totalWhiteCans + " банок белой краски и " + totalBrownCans + " банок коричневой краски");
 
-        var residue = secondBoxer % firstBoxer;
-        System.out.println(residue);
+        short bananaWeight = 80;
+        short milkWeight = 100;
+        short iceCreamWeight = 100;
+        short eggWeight = 70;
+        short bananas = 5;
+        short milkVolume = 200;
+        short eggs = 4;
+        short iceCreamBriquette = 2;
+        int totalBananaWeight = bananas * bananaWeight;
+        int totalMilkWeight = (milkVolume * milkWeight) / 100;
+        int totalIceCreamWeight = iceCreamBriquette * iceCreamWeight;
+        int totalEggsWeight = eggs * eggWeight;
+        int totalBreakfastWeight = totalBananaWeight + totalMilkWeight + totalIceCreamWeight + totalEggsWeight;
+        System.out.println(totalBreakfastWeight);
 
-        var totalTime = 640;
-        var firstTimeWork = 8;
-        var totalEmployers = totalTime / firstTimeWork;
-        System.out.println("Всего работников в компании - " + totalEmployers + " человек");
+        int weightNeedToLostKg = 7;
+        int weightNeedToLostGram = weightNeedToLostKg * 1000;
+        int firstDiet = 250;
+        int secondDiet = 500;
+        int firstDietDayNeed = weightNeedToLostGram / firstDiet;
+        int secondDietDayNeed = weightNeedToLostGram / secondDiet;
+        int avgDayNeed = (firstDietDayNeed + secondDietDayNeed) / 2;
+        System.out.println(weightNeedToLostGram);
+        System.out.println(firstDietDayNeed);
+        System.out.println(secondDietDayNeed);
+        System.out.println(avgDayNeed);
 
-        var newTotalEmployers = totalEmployers + 94;
-        var newTotalWork = newTotalEmployers * firstTimeWork;
-        System.out.println("Если в компании работает " + newTotalEmployers + " человек, то всего " + newTotalWork + " часов работы может быть поделено между сотрудниками");
+        int salary1 = 67760;
+        int salary2 = 83690;
+        int salary3 = 76230;
+        double newSalary1 = salary1 * 1.1;
+        double newSalary2 = salary2 * 1.1;
+        double newSalary3 = salary3 * 1.1;
+        double incomeDifference1 = newSalary1 - salary1;
+        double incomeDifference2 = newSalary2 - salary2;
+        double incomeDifference3 = newSalary3 - salary3;
+        System.out.println("Маша теперь получает " + newSalary1 + " рублей. Годовой доход вырос на " + incomeDifference1 + " рублей.");
+        System.out.println("Денис теперь получает " + newSalary2 + " рублей. Годовой доход вырос на " + incomeDifference2 + " рублей.");
+        System.out.println("Кристина теперь получает " + newSalary3 + " рублей. Годовой доход вырос на " + incomeDifference3 + " рублей.");
     }
 }
