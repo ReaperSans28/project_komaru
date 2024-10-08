dc976e5fc90071a053d34f0ad48787c1af8d0d54
ReaperSans28 <smileuwu321@gmail.com>
2024-10-08 10:27:19 +0300

    hw6 changes

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,55 @@
- class Main {
+class Main {
     public static void main(String[] args) {
         // 1 task
-        for (int i = 1; i <= 10; i ++) System.out.println(i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
 
         // 2 task
-        for (int i = 10; i > 0; i --) System.out.println(i);
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
 
         // 3 task
-        for (int i = 0; i < 17; i ++) System.out.println(i);
+        for (int i = 0; i < 17; i++) {
+            System.out.println(i);
+        }
 
         // 4 task
-        for (int i = 10; i >= -10; i --) System.out.println(i);
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
 
         // 5 task
-        for (int year = 1904; year <= 2096; year = year + 4) System.out.println(year + " год является високосным");
+        for (int year = 1904; year <= 2096; year = year + 4) {
+            System.out.println(year + " год является високосным");
+        }
 
         // 6 task
-        for (int i = 7; i <= 98; i += 7) System.out.println(i);
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.println(i);
+        }
 
         // 7 task
-        for (int i = 1; i <= 512; i *= 2) System.out.println(i);
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.println(i);
+        }
 
         // 8 task
-        int a = 29000;
-        for (int i = 1; i <= 12; i ++, a += 29000) System.out.println("Месяц " + i + ", сумма накоплений равна " + a + " рублей");
+        int savings1 = 29000;
+        for (int i = 1; i <= 12; i++, savings1 += 29000) {
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + savings1 + " рублей");
+        }
 
         // 9 task
-        double b = 29000;
-        for (int i = 1; i <= 12; i ++, b *= 1.01) System.out.println("Месяц " + i + ", сумма накоплений равна " + b + " рублей");
+        double savings2 = 29000;
+        for (int i = 1; i <= 12; i++, savings2 *= 1.01) {
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + savings2 + " рублей");
+        }
 
         // 10 task
-        for (int multiplier = 1; multiplier <= 10; multiplier ++) System.out.println("2 * " + multiplier + " = " + (2 * multiplier));
+        for (int multiplier = 1; multiplier <= 10; multiplier++) {
+            System.out.println("2 * " + multiplier + " = " + (2 * multiplier));
+        }
     }
 }
