c746d82ef3b0d3a69aa5987b49c64ea1878adda7
ReaperSans28 <smileuwu321@gmail.com>
2024-10-11 09:39:30 +0300

    hw8 changes

--- src/Main.java
+++ src/Main.java
@@ -20,14 +20,14 @@ class Main {
         }
 
         // 3 task
-        for (int i = integer.length - 1; i >= 0; i--) {
-            System.out.print(integer[i] + ", ");
+        for (int i = short1.length - 1; i >= 0; i--) {
+            System.out.print(short1[i] + ", ");
         }
         for (int i = double1.length - 1; i >= 0; i--) {
             System.out.print(double1[i] + ", ");
         }
-        for (int i = short1.length - 1; i >= 0; i--) {
-            System.out.print(short1[i]);
+        for (int i = integer.length - 1; i >= 0; i--) {
+            System.out.print(integer[i]);
             if (i > 0) {
                 System.out.print(", ");
             }
@@ -37,9 +37,9 @@ class Main {
         // 4 task
         for (int j : integer) {
             if (j % 2 != 0) {
-                System.out.print((j + 1) + " ");
+                System.out.print((j + 1));
             } else {
-                System.out.print(j + " ");
+                System.out.print(j);
             }
         }
     }
