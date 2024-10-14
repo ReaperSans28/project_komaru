98b24a4fd4851340f092a2466166f578a9f7a302
ReaperSans28 <smileuwu321@gmail.com>
2024-10-14 12:08:29 +0300

    hw10

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,26 @@
 class Main {
     public static void main(String[] args) {
         // 1 task
-        int[] spending = new int[]{10000, 9000, 40000, 5000, 7000};
-        int spendingSum = 0;
-        for (int j : spending) {
-            spendingSum += j;
-        }
-        System.out.println("Сумма трат за месяц составила " + spendingSum + " рублей");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
         // 2 task
-        int[] spending2 = new int[]{10000, 9000, 40000, 5000, 7000};
-        int maxSpending = 0;
-        int minSpending = spending2[0];
-        for (final int current : spending2) {
-            if (current > maxSpending) {
-                maxSpending = current;
-            }
-            if (current < minSpending) {
-                minSpending = current;
-            }
-        }
-        System.out.println("Минимальная сумма трат за неделю составила " + minSpending + " рублей. Максимальная сумма трат за неделю составила " + maxSpending + " рублей");
+        String fullNameUpper1 = fullName.replace("v", "V");
+        String fullNameUpper2 = fullNameUpper1.replace("a", "A");
+        String fullNameUpper3 = fullNameUpper2.replace("n", "N");
+        String fullNameUpper4 = fullNameUpper3.replace("o", "O");
+        String fullNameUpper5 = fullNameUpper4.replace("i", "I");
+        String fullNameUpper6 = fullNameUpper5.replace("c", "C");
+        String fullNameUpper = fullNameUpper6.replace("h", "H");
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUpper);
 
         // 3 task
-        int[] spending3 = new int[]{10001, 9000, 40000, 5000, 7000};
-        double spendingSum2 = 0;
-        for (int j : spending3) {
-            spendingSum2 += j;
-        }
-        System.out.println("Средняя сумма трат за месяц составила " + spendingSum2 / spending3.length + " рублей");
-
-        // 4 task
-        char[] reverseName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-
-        for (int i = reverseName.length - 1; i >= 0; i--) {
-            System.out.print(reverseName[i]);
-        }
+        String fullName2 = "Иванов Семён Семёнович";
+        String changedSecondName = fullName2.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + changedSecondName);
     }
 }
